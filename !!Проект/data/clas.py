be701601eee7f05a7c0d022d616d6cbb674a9391
Drakon-top <57674687+Drakon-top@users.noreply.github.com>
2020-05-11 17:07:43 +0300

    Add files via upload

---
+++ !!Проект/data/clas.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+
+import datetime
+import sqlalchemy
+from flask import request
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, PasswordField, BooleanField, SelectField
+from wtforms.validators import DataRequired, Email
+from wtforms.fields.html5 import EmailField
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, 
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, 
+                              index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime, 
+                                     default=datetime.datetime.now)
+    components = orm.relation("Reviews", back_populates='user')
+    
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)    
+    
+    
+class Reviews(SqlAlchemyBase):
+    __tablename__ = 'reviews'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, 
+                           primary_key=True, autoincrement=True)
+    estimation = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    types = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime, 
+                                     default=datetime.datetime.now)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer, 
+                                sqlalchemy.ForeignKey("users.id"))
+    user = orm.relation('User')
+    
+    
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    name = StringField('Имя пользователя', validators=[DataRequired()])
+    submit = SubmitField('Войти')    
+    
+    
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')  
+    
+    
+class Profile(FlaskForm):
+    submit = SubmitField('Выйти') 
+    
+    
+class ReviewsForm(FlaskForm):
+    title = StringField('Заголовок', validators=[DataRequired()])
+    content = TextAreaField("Содержание")
+    types = TextAreaField("Содержание")   
+    
+    
+class Tovar(SqlAlchemyBase):
+    __tablename__ = 'tovars'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, 
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    specifications = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    types = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    manufacturer = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime, 
+                                     default=datetime.datetime.now)
---
+++ !!Проект/start.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+
+
+from flask import Flask, render_template, request, redirect, url_for
+from data import db_session
+from data.clas import User
+from data.clas import Reviews
+from data.clas import RegisterForm
+from data.clas import LoginForm
+from data.clas import ReviewsForm
+from data.clas import Tovar
+from data.clas import Profile
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+db_session.global_init("db/base.sqlite")
+
+
+@app.route("/")
+@app.route('/index')
+def index(): 
+    session = db_session.create_session()
+    component = session.query(Tovar) 
+    return render_template("tovars.html", component=component)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    session = db_session.create_session()
+    return session.query(User).get(user_id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        user = session.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+
+@app.route('/component',  methods=['GET', 'POST'])
+def component():
+    if request.method == 'GET':
+        session = db_session.create_session()
+        proz = session.query(Tovar).filter(Tovar.types == 'Процессор')
+        mat = session.query(Tovar).filter(Tovar.types == 'Материнская плата')
+        video = session.query(Tovar).filter(Tovar.types == 'Видеокарта')
+        pam = session.query(Tovar).filter(Tovar.types == 'Оперативная память')
+        block = session.query(Tovar).filter(Tovar.types == 'Блок питания')
+        korp = session.query(Tovar).filter(Tovar.types == 'Корпус')
+        oxl = session.query(Tovar).filter(Tovar.types == 'Охлаждение')
+        ssd = session.query(Tovar).filter(Tovar.types == 'SSD')
+        disk = session.query(Tovar).filter(Tovar.types == 'Жесткий диск')
+        return render_template('Content.html', proz=proz, mat=mat, video=video, 
+                               pam=pam, block=block, korp=korp, oxl=oxl, ssd=ssd,
+                               disk=disk)
+    elif request.method == 'POST':
+        session = db_session.create_session()
+        component = Reviews()
+        component.title = request.form['title']
+        component.content = request.form['about']
+        component.types = request.form['class']
+        component.user_id = current_user.id
+        session.add(component)
+        session.commit()
+        return redirect('/index')  
+    
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+
+@app.route('/profile',  methods=['GET', 'POST'])
+def profile():
+    if request.method == 'GET':
+        session = db_session.create_session()
+        component = session.query(Reviews).filter(Reviews.user_id == current_user.id)    
+        return render_template('profile.html', component=component)        
+    elif request.method == 'POST':
+        return redirect('/logout')
+
+
+
+@app.route('/product')
+def product():
+    session = db_session.create_session()
+    component = session.query(Tovar)
+    return render_template('tovars.html', component=component)
+
+
+@app.route('/product/<types>')
+def product_typ(types):
+    session = db_session.create_session()
+    component = session.query(Tovar).filter(Tovar.types == types)
+    return render_template('tovars.html', component=component)
+
+
+@app.route('/product/<types>/<manufacturer>')
+def product_typ_man(types, manufacturer):
+    session = db_session.create_session()
+    component = session.query(Tovar).filter(Tovar.types == types, 
+                                            Tovar.manufacturer == manufacturer)
+    return render_template('tovars.html', component=component)
+
+
+@app.route('/tovar/<name>')
+def tovars(name):
+    session = db_session.create_session()
+    component = session.query(Tovar).filter(Tovar.name == name)
+    return render_template('see.html', component=component)
+
+
+def main():
+    app.run(port=8070, host='127.0.0.1')
+
+
+if __name__ == '__main__':
+    main()
